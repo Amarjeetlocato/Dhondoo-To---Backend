c6e6c61010a27a747a3356160d54045542575587
Amarjeetlocato <aksooon098098@gmail.com>
2026-09-09 12:58:36 +0530

    Configure User Registry Kafka SASL authentication

--- userRegistry/src/main/java/com/whoami/launch/config/KafkaProducerConfig.java
+++ userRegistry/src/main/java/com/whoami/launch/config/KafkaProducerConfig.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,15 +23,55 @@ public class KafkaProducerConfig {
 
         config.put(
                 ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                System.getenv().getOrDefault("KAFKA_SERVERS", "kafka:9092"));
+                System.getenv().getOrDefault(
+                        "KAFKA_SERVERS",
+                        "kafka:9092"
+                )
+        );
+
+        // ====================================
+        // KAFKA SASL AUTHENTICATION
+        // ====================================
+
+        config.put(
+                "security.protocol",
+                System.getenv().getOrDefault(
+                        "KAFKA_SECURITY_PROTOCOL",
+                        "SASL_PLAINTEXT"
+                )
+        );
+
+        config.put(
+                SaslConfigs.SASL_MECHANISM,
+                System.getenv().getOrDefault(
+                        "KAFKA_SASL_MECHANISM",
+                        "SCRAM-SHA-512"
+                )
+        );
+
+        String username = System.getenv("KAFKA_USERNAME");
+        String password = System.getenv("KAFKA_PASSWORD");
+
+        config.put(
+                SaslConfigs.SASL_JAAS_CONFIG,
+                "org.apache.kafka.common.security.scram.ScramLoginModule required "
+                        + "username=\"" + username + "\" "
+                        + "password=\"" + password + "\";"
+        );
+
+        // ====================================
+        // SERIALIZERS
+        // ====================================
 
         config.put(
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
+                StringSerializer.class
+        );
 
         config.put(
                 ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                JsonSerializer.class);
+                JsonSerializer.class
+        );
 
         return new DefaultKafkaProducerFactory<>(config);
     }
@@ -40,4 +81,3 @@ public class KafkaProducerConfig {
         return new KafkaTemplate<>(producerFactory());
     }
 }
-
